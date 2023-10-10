ec625c1d3d86aa481f8403ed34b032246b8f18e7
Niclas van Eyk <niclas.eyk@gmail.com>
2023-10-10 20:00:59 +0200

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import mdx from "@astrojs/mdx";
 import serviceWorker from "astrojs-service-worker";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://niclasve.me",
   integrations: [mdx(), sitemap(), serviceWorker()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
