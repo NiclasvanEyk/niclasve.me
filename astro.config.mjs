c2b870e6208409c5a1ba25ab25427a5fa10ed80a
Niclas van Eyk <niclas.eyk@gmail.com>
2024-02-26 18:53:30 +0100

    add expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import autolinkHeadings from 'rehype-autolink-headings';
-
 import react from "@astrojs/react";
+
+import expressiveCode from "astro-expressive-code";
+
+const expressiveCodeIntegration = expressiveCode({
+  themes: ['dark-plus', 'light-plus'],
+})
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://niclasve.me",
-  integrations: [mdx(), sitemap(), serviceWorker(), tailwind(), react()],
+  integrations: [sitemap(), serviceWorker(), tailwind(), react(), expressiveCodeIntegration, mdx()],
   output: "server",
   markdown: {
     rehypePlugins: [rehypeHeadingIds, [autolinkHeadings, {
