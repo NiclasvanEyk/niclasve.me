adec99927e060171ff7decab4faf3dab8f3ea67e
Niclas van Eyk <niclas.eyk@gmail.com>
2025-01-04 13:44:07 +0100

    migrate the hovercards to ariakit
    Makes it easier to keep the maps popup mounted between opens, so the
    iframe does not have to load everytime

--- src/components/Homepage/HoverCards/HoverableText.tsx
+++ src/components/Homepage/HoverCards/HoverableText.tsx
@@ -1,4 +1,4 @@
-import * as HoverCard from '@radix-ui/react-hover-card';
+import { HovercardProvider, Hovercard, HovercardAnchor } from "@ariakit/react";
 import type { ReactNode } from 'react';
 
 interface Props {
@@ -9,21 +9,15 @@ interface Props {
 }
 
 export function HoverableText({ text, children, width, padding }: Props) {
-  const animations = "data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all";
   let applyPadding = padding === undefined ? true : padding;
 
-  return <HoverCard.Root openDelay={0.25}>
-    <HoverCard.Trigger asChild>
-      <a className="underline cursor-help">{text}</a>
-    </HoverCard.Trigger>
+  return <HovercardProvider timeout={250}>
+    <HovercardAnchor className="underline cursor-help">
+      {text}
+    </HovercardAnchor>
 
-    <HoverCard.Portal>
-      <HoverCard.Content
-        side="top"
-        className={`bg-zinc-50 overflow-hidden dark:bg-zinc-800 rounded-md ${applyPadding ? 'p-5' : ''} ${width} ${animations} shadow-md`}
-      >
-        {children}
-      </HoverCard.Content>
-    </HoverCard.Portal>
-  </HoverCard.Root>;
+    <Hovercard className={`data-[state=inactive]:hidden bg-zinc-50 overflow-hidden dark:bg-zinc-800 rounded-md ${applyPadding ? 'p-5' : ''} ${width} shadow-md z-50`}>
+      {children}
+    </Hovercard>
+  </HovercardProvider>;
 }
